2569853dd17f099c4b555ded78f44790968f906b
las139 <qqwer153@gmail.com>
2024-02-11 20:42:31 +0900

    chore: 초기 프로젝트 환경 구성

---
+++ src/main/java/com/lsm/task/PayhereTaskApplication.java
@@ -0,0 +1,13 @@
+package com.lsm.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayhereTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayhereTaskApplication.class, args);
+	}
+
+}
